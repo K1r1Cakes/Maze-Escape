4e97cdef8027128d6dcba24493db0231bcbc6d5f
K1r1Cakes <akiriachetty4@gmail.com>
2026-03-03 20:35:52 +0200

    Main | Set enemy to spawn at certain trigger

--- Maze Escape/Assets/Scripts/Enemy_Trigger.cs
+++ Maze Escape/Assets/Scripts/Enemy_Trigger.cs
@@ -2,25 +2,14 @@ using UnityEngine;
 
 public class Enemy_Trigger : MonoBehaviour
 {
-
     public GameObject enemy;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void TriggerOnEnter2D(Collider2D collide)
+    private void OnTriggerEnter2D(Collider2D collide)
     {
         if (collide.CompareTag("Player"))
         {
             Debug.Log("Enemy will spawn");
+            enemy.SetActive(true);
         }
     }
 }

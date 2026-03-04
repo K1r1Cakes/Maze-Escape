cc52039081dbd8c9de1ec0065fa01815071ef75c
K1r1Cakes <akiriachetty4@gmail.com>
2026-03-04 22:19:31 +0200

    Main | Panel pops up when player escapes

--- Maze Escape/Assets/Scripts/Escape_Zone.cs
+++ Maze Escape/Assets/Scripts/Escape_Zone.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Escape_Zone : MonoBehaviour
 {
+    public GameObject panel;
+    public string[] Win;
+
     private void OnTriggerEnter2D(Collider2D collide)
     {
         if (collide.CompareTag("Player"))
         {
             Debug.Log("Player Escaped!");
+            panel.SetActive(true);
         }
     }
 }

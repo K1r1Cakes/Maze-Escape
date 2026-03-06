ef26faafaa4cea9e8f13a7a05c145c1792f64416
K1r1Cakes <akiriachetty4@gmail.com>
2026-03-06 16:18:17 +0200

    Main | Panels appearing for win/loss

--- Maze Escape/Assets/Scripts/New_Enemy.cs
+++ Maze Escape/Assets/Scripts/New_Enemy.cs
@@ -5,7 +5,14 @@ public class New_Enemy : MonoBehaviour
 {
    public Transform target;
    private NavMeshAgent agent;
+    public GameObject panel;
+    public GameObject loseText;
+     Rigidbody2D rb;
 
+     private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -16,6 +23,25 @@ public class New_Enemy : MonoBehaviour
     void Update()
     {
         agent.SetDestination(target.position);
+    }
+
+    void FixedUpdate()
+    {
+         if (PauseController.isGamePaused)
+        {
+            rb.linearVelocity = Vector2.zero;
+            return;
+        }
+    }
+
+     private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Caught Player");
+            panel.SetActive(true);
+            loseText.SetActive(true);
+        }
     }
 }
 

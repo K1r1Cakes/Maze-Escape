40f7eb31a04e14fa6db08d8f7e7c4a6dce79e2da
K1r1Cakes <akiriachetty4@gmail.com>
2026-03-07 23:07:03 +0200

    Main | Added Reset button

--- Maze Escape/Assets/Scripts/UI.cs
+++ Maze Escape/Assets/Scripts/UI.cs
@@ -1,7 +1,20 @@
+
 using UnityEngine;
 
 public class UI : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject enemy;
+    public GameObject panel;
+
+    Vector3 playerPos;
+    Vector3 enemyPos;
+
+    void Start()
+    {
+        playerPos = player.transform.position;
+        enemyPos = enemy.transform.position;
+    }
     public void Exit()
     {
         Debug.Log("Quit");
@@ -10,5 +23,15 @@ public class UI : MonoBehaviour
     #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
     #endif 
+    }
+
+    public void Reset()
+    {
+        Debug.Log("Reset");
+        player.transform.position = playerPos;
+        enemy.transform.position = enemyPos;
+        enemy.SetActive(false);
+        panel.SetActive(false);
+        PauseController.setPause(false);
     }
 }
